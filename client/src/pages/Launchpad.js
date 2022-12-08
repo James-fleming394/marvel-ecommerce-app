2a9d16f8997007f8f9090bab96dd5540c835b738
james-fleming394 <james.fleming394@gmail.com>
2022-12-08 11:42:08 -0500

    Launchpad CSS

--- client/src/pages/Launchpad.js
+++ client/src/pages/Launchpad.js
@@ -7,18 +7,13 @@ const LaunchPad = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="Launchpad">
+        <div className="launchpad">
             <div className="text-space">
                 <div className="title">
                     <h1>Marvel Engine</h1>
                 </div>
-                <div className="desc">
-                    <h3>Welcome to Marvel Engine!  An Ecommerce Web Application dedicated to fans of 
-                    Marvel and Marvel Comics!  Shop online for your favorite comics, toys, movies and more!  
-                    All at the click of a button!</h3>
-                </div>
-                <div className="launch-button">
-                    <button onClick={() => navigate('/home')}>Enter Here</button>
+                <div>
+                    <button className="launch-button" onClick={() => navigate('/home')}><span>Enter Here  </span></button>
                 </div>
             </div>
         </div>
