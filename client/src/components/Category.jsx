b301901a8d81797f99c7652edd02f584fcd3e26e
james-fleming394 <james.fleming394@gmail.com>
2022-12-09 15:17:07 -0500

    Product Categories CSS

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -5,21 +5,21 @@ const Category = () => {
     const categories = [
         {
             id: 1,
-            img: "https://i.imgur.com/CdBAPgm.jpg",
+            img: "https://i.pinimg.com/564x/64/8a/9a/648a9a506a5a850088568599a3f707c5.jpg",
             title: "Shop Comics"
         },
         {
             id: 2,
-            img: "https://i.imgur.com/yZKLwF1.jpg",
+            img: "https://i.pinimg.com/564x/d1/c9/28/d1c928d2bafb09fdcf6aa22ca60a2031.jpg",
             title: "Shop Toys"
         },
         {
             id: 3,
-            img: "https://i.imgur.com/rxnVkah.jpg",
+            img: "https://i.pinimg.com/564x/ec/6a/c8/ec6ac86e37b16a15f92aa8df85ae7a61.jpg",
             title: "Shop Apparel"
         }
     ]
-    
+
     return (
         <div>
             {categories.map(item => (
--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const CategoryItem = ({item}) => {
     return (
+        <div className="categories">
         <div className="product-category">
             <img className="category-image" src={item.img} alt="product" />
-            <div>
+            <div className="info">
             <h1>{item.title}</h1>
-            <button>Shop Now</button>
+            <button className="category-button">Shop Now</button>
             </div>
+        </div>
         </div>
     )
 }
