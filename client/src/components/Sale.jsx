4b24ad91d8c9c436193a783396ba88364efb5193
james-fleming394 <james.fleming394@gmail.com>
2022-12-09 13:44:12 -0500

    Home page CSS

--- client/src/components/Sale.jsx
+++ client/src/components/Sale.jsx
@@ -7,8 +7,6 @@ const Sale = () => {
         <div className="home-page">
             <div className="home-title">
                 <h1>Welcome to Marvel Engine!</h1>
-                <h2>Marvel Engine is the home of countless Marvel Products for you to view and purchase,
-                    check out our sales below!</h2>
             </div>
         </div>
         <div className="home-page2">
@@ -16,12 +14,12 @@ const Sale = () => {
                     <h1>Today's Sales</h1><span>Don't miss out on these amazing deals!</span>
                     <h3>30% off!</h3>
                     <div className="sale-items">
-                        <img src="https://m.media-amazon.com/images/I/61bbm-+wxwL._SX418_BO1,204,203,200_.jpg" alt="marvel book" />
-                        <button>Shop Now</button>
-                        <img src="https://m.media-amazon.com/images/I/61e8-nPFoZL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="marvel book" />
-                        <button>Shop Now</button>
-                        <img src="https://m.media-amazon.com/images/I/61VhOdqZWmL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="marvel book" />
-                        <button>Shop Now</button>
+                        <img className="sale-pic" src="https://m.media-amazon.com/images/I/61bbm-+wxwL._SX418_BO1,204,203,200_.jpg" alt="marvel book" />
+                        <button className="sale-button">Shop Now</button>
+                        <img className="sale-pic" src="https://m.media-amazon.com/images/I/61e8-nPFoZL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="marvel book" />
+                        <button className="sale-button">Shop Now</button>
+                        <img className="sale-pic" src="https://m.media-amazon.com/images/I/61VhOdqZWmL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="marvel book" />
+                        <button className="sale-button">Shop Now</button>
                     </div>
                 </div>
             </div>
