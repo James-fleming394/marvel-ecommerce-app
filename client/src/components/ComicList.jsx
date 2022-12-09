082340c2accd3b08f0d8cdb0a8b0f4914dce9dcb
james-fleming394 <james.fleming394@gmail.com>
2022-12-09 18:05:22 -0500

    Back button

--- client/src/components/ComicList.jsx
+++ client/src/components/ComicList.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const ComicList = ({item}) => {
+
+    const navigate = useNavigate();
+
     return (
         <div className="comics-product-page">
-            <div className="comics-product">
-                <img className="comics-image" src={item.img} alt="comic book cover" />
-            <div className="comic-info">
-                <h2>{item.name}</h2>
-                <h3>{item.price}</h3>
-                <button className="comic-button">Add to Cart</button>
+                <div className="comics-product">
+                    <img className="comics-image" src={item.img} alt="comic book cover" />
+                    <div className="comic-info">
+                        <h2>{item.name}</h2>
+                        <h3>{item.price}</h3>
+                        <button className="comic-button">Add to Cart</button>
+                        <button className="back-button" onClick={() => navigate(-1)}>Go Back</button>
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
     )
 }
 
