e522e4eea197923ec7b16c443a57c8ac51c438dc
james-fleming394 <james.fleming394@gmail.com>
2022-12-12 13:47:04 -0500

    Comics routes set up

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const router = Router();
+const controllers = require('../controllers');
+
+// Home 
+router.get('/', (req, res) => {
+    res.send("welcome Home")
+;})
+
+// Comic Products
+
+router.get('/products-comics', controllers.getAllComics)
+router.post('/products-comics', controllers.createComic)
+router.get('/products-comics/:id', controllers.getComicById)
+
