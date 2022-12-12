ec6eab2aa291452f34b455d67bb99a56a23a4434
james-fleming394 <james.fleming394@gmail.com>
2022-12-12 10:43:27 -0500

    Products controllers set up

---
+++ controllers/index.js
@@ -0,0 +1,29 @@
+const db = require("../db");
+const { Product, Cart } = require('../models');
+
+//Products 
+
+const getAllProducts = async (req, res) => {
+    const product = await Product.find()
+    res.send(product);
+}
+
+const getProductById = async (req, res) => {
+    const {id} = req.params
+    const product = await Product.findById(id)
+    res.send(product)
+}
+
+const createProduct = async (req,res) => {
+    const product = await new Product(req.body)
+    await product.save()
+    res.send(product)
+    console.log("New Product Created!")
+}
+
+
+module.exports = {
+    getAllProducts,
+    getProductById,
+    createProduct
+}
