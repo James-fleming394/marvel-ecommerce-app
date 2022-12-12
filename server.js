ee58a4d0e702dacb3c82b9064bb1c6f69984ccca
james-fleming394 <james.fleming394@gmail.com>
2022-12-12 13:40:29 -0500

    Fix on server.js

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+const db = require("./db");
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+});
