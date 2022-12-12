524dfd809651f8af927bc75c8b3ffb746a7ef91a
james-fleming394 <james.fleming394@gmail.com>
2022-12-12 12:28:09 -0500

    Auth routes working

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const PORT = process.env.PORT || 5001;
 const db = require('./db');
 const cors = require("cors");
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth")
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+app.use('/api/auth', authRoute)
 app.use('/api/user', userRoute)
 
 app.listen(PORT, () => {
