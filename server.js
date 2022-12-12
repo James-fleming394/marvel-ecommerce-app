c846472e61d12719478f874f14db6eb36107a5aa
james-fleming394 <james.fleming394@gmail.com>
2022-12-12 09:32:26 -0500

    .env file set up and connect

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-mongoose.connect("mongodb+srv://jamesfleming394:jamesfleming394@cluster0.bwkdphq.mongodb.net/?retryWrites=true&w=majority")
-.then(() => console.log("Successfully connected to MongoDB Servers")).catch((err) =>console.log(err));
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Successfully connected to MongoDB Servers"))
+    .catch((err) =>console.log(err));
 
 app.listen(5001, () => {
     console.log("Connected to server");
