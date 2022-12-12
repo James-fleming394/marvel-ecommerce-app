a9bb01e1494d2448f2e036ecb9ed96918b41ddf7
james-fleming394 <james.fleming394@gmail.com>
2022-12-12 12:37:14 -0500

    Hashing of password complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const User = require("../models/user")
+const User = require("../models/user");
+const Cryptojs = require("crypto-js")
 
 //Register 
 
@@ -7,7 +8,7 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: Cryptojs.AES.encrypt(req.body.password, process.env.PASSWORD_SECRET).toString(),
     });
     try {
         const savedUser = await newUser.save();
