5aeeaa3be5f8348d2019a58838dc3ed60d6c7e31
james-fleming394 <james.fleming394@gmail.com>
2022-12-13 15:20:26 -0500

    Apparel pages set up

---
+++ client/src/components/ApparelPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+const ApparelPage = () => {
+
+}
+
+export default ApparelPage
---
+++ client/src/components/ApparelProducts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useNavigate, useParams } from "react-router-dom";
+
+const AllApparelProducts = () => [
+
+]
+
+export default AllApparelProducts
