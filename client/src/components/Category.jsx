8d045d1094d4ef17b537b2486e95d2a004dd1c13
james-fleming394 <james.fleming394@gmail.com>
2022-12-13 18:40:51 -0500

    Cart Model update

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -20,11 +20,25 @@ const Category = () => {
                 <h1 onClick={() => navigate("/toys")}>Shop Toys</h1>
             </div>
             <img className="category-image" 
-            src="https://i.pinimg.com/564x/d1/c9/28/d1c928d2bafb09fdcf6aa22ca60a2031.jpg" />
+            src="https://cdn.wallpapersafari.com/56/34/NG1oO0.jpg" />
         </div>
         <div className="product-category">
         <div className="title-category">
                 <h1 onClick={() => navigate("/apparel")} >Shop Apparel</h1>
+            </div>
+            <img className="category-image" 
+            src="https://i.pinimg.com/564x/d1/c9/28/d1c928d2bafb09fdcf6aa22ca60a2031.jpg" />
+        </div>
+        <div className="product-category">
+        <div className="title-category">
+                <h1 onClick={() => navigate("/apparel")} >Shop Movies</h1>
+            </div>
+            <img className="category-image" 
+            src="https://wallpapercave.com/wp/wp4330404.jpg" />
+        </div>
+        <div className="product-category">
+        <div className="title-category">
+                <h1 onClick={() => navigate("/apparel")} >Shop Shows</h1>
             </div>
             <img className="category-image" 
             src="https://i.pinimg.com/564x/ec/6a/c8/ec6ac86e37b16a15f92aa8df85ae7a61.jpg" />
--- models/cart.js
+++ models/cart.js
@@ -2,18 +2,9 @@ const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema(
     {
-        userId: { type: String, required: true },
-        products: [
-            {
-                productId: {
-                    type: String,
-                },
-                quantity: {
-                    type: Number,
-                    default: 1,
-                }
-            }
-        ]
+        comic: [{type: Schema.Types.ObjectId, ref: 'Comic'}],
+        toy: [{type: Schema.Types.ObjectId, ref: 'Toy'}],
+        apparel: [{type: Schema.Types.ObjectId, ref: 'Apparel'}]
     },
     { timestamps: true }
 )
